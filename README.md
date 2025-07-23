16f3e1db66d23bbc51d507050c185f233a6f759d
Vedant Patil <vedant.waman.patil@gmail.com>
2025-07-22 21:43:20 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bias Engine
+
+The goal of this project is to be able to determine how risky a company is based on the recent news, the way we hope to accomplish this is by utilizing langchain to connect with a llm and have a mcp server for our llm to scrape the internet for relevant news articles.
+
+After this we then have a NLP process to determine the bias and overall come to a evaluation using a algorithm to conclude the risk of investing in the company
---
+++ webscraping/scrape.go
@@ -0,0 +1,21 @@
+package main
+
+import "github.com/gocolly/colly"
+
+type Article struct {
+	comapany, url, text string
+	bias, confidence    float64
+}
+
+func main() {
+	companiesToScrape := []string{"microsoft", "nvidia"}
+	visitedUrls := make(map[string]bool)
+
+	c := colly.NewCollector(
+		colly.Async(true),
+	)
+
+	c.Limit(&colly.LimitRule{Parallelism: 4, DomainGlob: "*"})
+
+	// Setup the scraping object need to now setup the scraping and crawling process of what we will do upon reaching each website and such
+}
